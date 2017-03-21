f41bb879654c45b38735f258271f8030d5796043
alicespyglass <alicespyglass@gmail.com>
2017-03-21 16:50:50 +0000

    completed ex11 - refactored in-transit to entry station

--- lib/feature_test.rb
+++ lib/feature_test.rb
@@ -2,9 +2,15 @@ require './lib/oystercard'
 
 card = Oystercard.new
 card.top_up(20)
-card.touch_in
-
-#When my journey is complete,
-#I need the correct amount deducted from my card
+card.touch_in("Bank")
+card.in_journey?
 card.touch_out
-card.balance
+card.in_journey?
+
+# In order to pay for my journey
+# As a customer
+# I need to know where I've travelled from
+card.touch_in
+<station>
+card
+<station>
--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -3,13 +3,13 @@ class Oystercard
   MAXIMUM_BALANCE = 90
   MINIMUM_TRAVEL_BALANCE = 1
 
-  attr_reader :balance, :fare, :history
+  attr_reader :balance, :fare, :entry_station
 
   def initialize(fare=MINIMUM_TRAVEL_BALANCE)
     @balance = 0
-    @in_transit = false
+    # @in_transit = false
     @fare = fare
-    @history = []
+    @entry_station = nil
   end
 
   def top_up(amount)
@@ -19,16 +19,15 @@ class Oystercard
 
   def touch_in(station)
     fail "Insufficient funds. £1 minimum needed to travel." if @balance < MINIMUM_TRAVEL_BALANCE
-    @in_transit = true
-    @history << station
+    @entry_station = station
   end
 
   def in_journey?
-    @in_transit
+    !!@entry_station
   end
 
   def touch_out
-    @in_transit = false
+    @entry_station = nil
     deduct(fare)
   end
 
