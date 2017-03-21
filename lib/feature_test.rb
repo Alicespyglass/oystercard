86bd8c5015a2c42d6b7a970190e6d2ccc8328b9b
alicespyglass <alicespyglass@gmail.com>
2017-03-21 12:09:23 +0000

    completed ex7 - implement deduct fare functionality

--- lib/feature_test.rb
+++ lib/feature_test.rb
@@ -2,3 +2,14 @@ require './lib/oystercard'
 
 card = Oystercard.new
 card.top_up(20)
+card.balance
+card.deduct(5)
+card.balance
+
+# I need my fare deducted from my card
+
+
+# deduct fare from card
+card.deduct(fare)
+
+# later - calculate fare
--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,7 +1,7 @@
 class Oystercard
 
   MAXIMUM_BALANCE = 90
-  
+
   attr_reader :balance
 
   def initialize
@@ -9,7 +9,12 @@ class Oystercard
   end
 
   def top_up(amount)
-    fail "Top up unsuccessful, balance cannot exceed limit of #{MAXIMUM_BALANCE}" if @balance + amount > MAXIMUM_BALANCE
+    fail "Top up unsuccessful, balance cannot exceed limit of #{MAXIMUM_BALANCE}" if balance + amount > MAXIMUM_BALANCE
     @balance += amount
   end
+
+  def deduct(fare)
+    @balance -= fare
+  end
+
 end
